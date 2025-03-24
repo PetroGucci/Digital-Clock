fa03e12b8a09a6812892be2e4d7052363476e4fa
PetroGucci <mauriciojvr0@gmail.com>
2025-03-24 16:18:35 -0700

    Mejora en la logica del reloj

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const $ = (selector) => document.querySelector(selector);
+const $all = (selector) => document.querySelectorAll(selector);
 
 const hour = $('.hour');
-const dot = $('.dot');
+const dots = $all('.dot'); 
 const min = $('.min');
+const sec = $('.sec');
 const week = $('.week');
 const locationInfo = $('.location-info'); 
 
@@ -16,9 +18,19 @@ function update() {
     const options = { weekday: 'short' };
     const localeTime = new Date(now.toLocaleString("en-US", { timeZone }));
 
-    dot.classList.toggle('invisible', showDot);
+    // Alternar visibilidad solo del segundo ':'
+    dots[1].classList.toggle('invisible', showDot);
+    dots[1].classList.toggle('visible', !showDot);
+
+    // Mantener el primer ':' estático o cambiarlo al cambiar el minuto
+    if (localeTime.getSeconds() === 0) {
+        dots[0].classList.remove('invisible');
+        dots[0].classList.add('visible');
+    }
+
     hour.textContent = String(localeTime.getHours()).padStart(2, '0');
     min.textContent = String(localeTime.getMinutes()).padStart(2, '0');
+    sec.textContent = String(localeTime.getSeconds()).padStart(2, '0');
 
     Array.from(week.children).forEach(ele => ele.classList.remove('active'));
     week.children[localeTime.getDay()].classList.add('active');
